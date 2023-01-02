53375b3369cd4687f01b0658be2f3024d0817582
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2023-01-02 06:04:59 +0530

    Prob4.java

---
+++ Prob4.java
@@ -0,0 +1,13 @@
+package Assign4;
+
+import java.util.Scanner;
+
+public class Prob4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer :");
+        int num = sc.nextInt();
+        if(num <0) System.out.println("The number is negative and skipped.");
+        else System.out.println(num);
+    }
+}
